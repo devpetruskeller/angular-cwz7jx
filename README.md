e5bd5970ee478f8785387526d42eeb6ff44bfd07
devpetruskeller <dev.petruskeller@gmail.com>
2022-01-11 10:33:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cwz7jx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cwz7jx)
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  /*
+  hero = 'Windstorm';
+  */
+  hero: Hero = {
+    id: 1,
+    name: 'Keller',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
